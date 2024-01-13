6a519cec5a6fff0a420b5bfd07844ff3766ec54a
Marina <marinasoon@gmail.com>
2024-01-13 16:24:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int vvod = 1100;
+        if (vvod > 1000) {
+            System.out.println(100 + vvod + (vvod/100));
+        } else {
+            System.out.println(100 + vvod);
+        }
+    }
+}
